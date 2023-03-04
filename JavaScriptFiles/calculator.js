ac43dc62d3da311d41a7a25a1b23bc5b3e366e76
Prem Gumathanavar <prem.gumathanavar@superzop.com>
2023-03-04 15:05:46 +0530

    EMIcalculations

--- JavaScriptFiles/calculator.js
+++ JavaScriptFiles/calculator.js
@@ -4,16 +4,27 @@ function sleep(ms) {
  }
 
 async function showResults(){
-
+    document.getElementById("results").style.display='none';
     document.getElementById("loading").style.display = 'block';
 
-    await sleep(2000);
+    await sleep(1000);
     displayresults();
     
 }
 
 function displayresults(){
-    let p = document.getElementById("")
+    let P = parseFloat(document.getElementById("principalAmount").value);
+    let n = parseFloat(document.getElementById("timePeriod").value)*12;
+    let R = parseFloat(document.getElementById("rateOfInterest").value)/100/12;
+    const EMI = document.getElementById("emi");
+    const totalAmount = document.getElementById("totalAmount");
+    const interestAmount = document.getElementById("interestAmount");
+    const x = Math.pow(1 + R, n);
+    const monthly = (P * x * R) / (x - 1);
+    let c=(n*monthly)-P;
+    totalAmount.value = n*monthly;
+    interestAmount.value = c;
+    EMI.value=monthly;
     document.getElementById("loading").style.display = 'none';
     document.getElementById("results").style.display='block';
 }
