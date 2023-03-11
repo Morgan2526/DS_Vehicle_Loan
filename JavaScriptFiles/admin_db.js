c781e6925829ad22351952b5d9adc54f82eecf8a
moonlight-blue <paramjangale@gmail.com>
2023-03-11 13:40:25 +0530

    Add admin db

---
+++ JavaScriptFiles/admin_db.js
@@ -0,0 +1,38 @@
+let admins = [
+    {
+        "first_name":"Sanjay",
+        "last_name":"Singh",
+        "id":"A1111",
+
+    }
+    ,
+    {
+        "first_name":"Suraj",
+        "last_name":"Singh",
+        "id":"A1112",
+
+    }
+    ,
+    {
+        "first_name":"Raj",
+        "last_name":"Rajput",
+        "id":"A1122",
+
+    }
+    ,
+    {
+        "first_name":"Aditya",
+        "last_name":"Roy",
+        "id":"A1166",
+
+    }
+    ,
+    {
+        "first_name":"Joseph",
+        "last_name":"Fernandes",
+        "id":"A1116",
+
+    }
+
+];
+
